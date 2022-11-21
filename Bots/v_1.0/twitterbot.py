98aab679d0abbfb73dc2ab2572b2091761ded25d
AM Digital Labs <118691148+AMDigitalLabs@users.noreply.github.com>
2022-11-21 18:52:31 -0500

    Update twitterbot.py
    Removed key info.

--- Bots/v_1.0/twitterbot.py
+++ Bots/v_1.0/twitterbot.py
@@ -2,10 +2,10 @@
 import tweepy
 
 # Variables that contains the credentials to access Twitter API
-ACCESS_TOKEN = '1594434951113482241-bFcSbpO2mnKq4UaOWonFue9IYc5Lei'
-ACCESS_SECRET = 'lJJBOwIRTnToV7PO9nIINqmQFQYFHVXHuj1tXOKCIHSqI'
-CONSUMER_KEY = 'SGh0whNXZWmq6jf8xUV72KcHK'
-CONSUMER_SECRET = 'fr68c6OoWVlyczgEOup4P84nUonApB2InjMjdUS59u7oJdygOt'
+ACCESS_TOKEN = ''
+ACCESS_SECRET = ''
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
 
 
 # Setup access to API
